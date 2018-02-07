331dc8091d59c39b92109670ce1241b30d780b5a
Peter Wong <wong978@usc.edu>
2018-02-06 18:40:44 -0800

    Playlists

--- resources/views/playlist-list.blade.php
+++ resources/views/playlist-list.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Playlists</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
-  <body>
-    <ul>
-        @foreach($playlists as $playlist)
-          <li>
-              <a href="/playlists/{{$playlist->PlaylistId}}">
-                  {{$playlist->Name}}
-              </a>
-          </li>
-        @endforeach
-    </ul>
-  </body>
-</html>
+@extends('main-layout')
+
+@section('content')
+<ul>
+    @foreach($playlists as $playlist)
+      <li>
+          <a href="/playlists/{{$playlist->PlaylistId}}">
+              {{$playlist->Name}}
+          </a>
+      </li>
+    @endforeach
+</ul>
+@endsection
